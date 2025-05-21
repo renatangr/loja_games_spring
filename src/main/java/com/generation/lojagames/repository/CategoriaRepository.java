a1e66166017e3698f67ded2ff5636cfa70327a9d
Renata Negrini <renatanegrinim@gmail.com>
2025-05-21 11:01:09 -0300

    fix: correção de annotation

--- src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.generation.lojagames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojagames.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	
 	List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package com.generation.lojagames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojagames.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
 	List<Produto> findAllByTituloContainingIgnoreCase(String titulo);
